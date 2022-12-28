de70acd4b66492837d4f54acd9e64a0d6f9a2c69
Francesco Leardini <piolag@gmail.com>
2022-12-28 17:12:43 +0100

    Added expressjs dev dependency

---
+++ app.js
@@ -0,0 +1,26 @@
+// **************************************************************************
+// ***  NOTE: This file is to be used with expressjs for test purpposes ***
+// **************************************************************************
+
+const express = require('express');
+const app = express();
+const router = express.Router();
+
+const path = __dirname + '/';
+const port = 3000;
+
+router.use(function (req,res,next) {
+  console.log('/' + req.method);
+  next();
+});
+
+router.get('/', function(req,res){
+  res.sendFile(path + 'index.html');
+});
+
+app.use(express.static(path));
+app.use('/', router);
+
+app.listen(port, function () {
+  console.log('App is now ready...')
+})
